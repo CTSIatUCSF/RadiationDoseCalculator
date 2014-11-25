df7d68ac791185c22b1f05148f9b00b1782bc53b
randall <randall@cranestylelabs.com>
2014-11-25 13:55:31 -0800

    Add route to data.json

--- node/index.js
+++ node/index.js
@@ -1,18 +1,22 @@
 var express = require("express");
-var app = express();
 var path = require( "path" );
+var app = express();
 
 
 app.get("/", function (req, res) {
   res.sendFile(path.resolve("html/index.html"));
 });
 
-app.get('/js/app.js', function(req,res){
+app.get("/js/app.js", function(req,res){
     res.sendFile(path.resolve("js/app.js"));
 });
 
-app.get('/css/styles.css', function(req,res){
+app.get("/css/styles.css", function(req,res){
     res.sendFile(path.resolve("css/styles.css"));
+});
+
+app.get("/js/data.json", function(req,res){
+    res.sendFile(path.resolve("js/data.json"));
 });
 
 var server = app.listen(3000, function () {

d6ad139613613350795fcdde80837cee60bd0475
randall <randall@cranestylelabs.com>
2014-11-26 16:08:35 -0800

    Read json data from file Create XRay form with working calculations

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,7 @@
 angular.module("RadCalc.controllers", []).controller("XRayFormCtrl", function($scope, getDataService) {
 
     $scope.forms = [{
+      id: "XRay",
       name: "X-ray Examinations",
       headers:["Study", "Examination", "# Scans", "Standard of Care?", "Gender Predominance", "EDE(mSv)"],
       exams:[ defaultTomographyExam(0) ]
@@ -11,7 +12,7 @@ angular.module("RadCalc.controllers", []).controller("XRayFormCtrl", function($s
     };
     
     $scope.submit = function(form){
-          console.log(form.exams);
+        console.log(form.exams);
     };
 
     $scope.socLabel = function(value) {
@@ -19,17 +20,63 @@ angular.module("RadCalc.controllers", []).controller("XRayFormCtrl", function($s
         return "No";
     };
 
+    $scope.calculatedEDE = function(form, exam) {
+        if (exam.exam === "" ||
+            exam.exam === undefined) {
+            return 0;
+        }
+        var singleScanEDE = $scope.getProcedurePropertyValue(form.id, exam.exam, exam.gender);
+        exam.ede = singleScanEDE * exam.scans;
+        return exam.ede;
+    };
+
+    $scope.getAllProcedures = function(categoryID) {
+        for (var categoryIndex in $scope.examData.DoseData) {
+            var category = $scope.examData.DoseData[categoryIndex];
+            if (category.name == categoryID) {
+                return category.exams;
+            }
+        }
+    };
+
+    $scope.getProcedure = function(categoryID, procedureName) {
+        var allProcedures = $scope.getAllProcedures(categoryID);
+        for (var procedureIndex in allProcedures) {
+            var procedure = allProcedures[procedureIndex];
+            if (procedure.name == procedureName) {
+                return procedure;
+            }
+        }
+    };
+
+    $scope.getAllProcedureProperties = function(categoryID, procedureName) {
+        var procedure = $scope.getProcedure(categoryID, procedureName);
+        return procedure.properties;
+    };
+
+    $scope.getProcedurePropertyValue = function(categoryID, procedureName, genderPredominance) {
+        var properties = $scope.getAllProcedureProperties(categoryID, procedureName);
+        for (var propertyIndex in properties) {
+            var property = properties[propertyIndex];
+            if (property.gender == genderPredominance) {
+                return property.value;
+            }
+        }
+    };
+
     $scope.examData = {};
     getDataService.getData().then(function (response) {
-        console.log(response.data.ConsentNarrative);
-        $scope.examData = response.data.DoseData.XRay;
+        $scope.examData = response.data;
     }, function (error) {
         console.error(error);
+    }).then(function() {
+        $scope.examData = getDataService.reformatJsonData($scope.examData);
+        console.log($scope.examData);
     });
 
     function defaultTomographyExam(currentCount) {
         currentCount++;
-        return { study_num: currentCount, exam: "", scans: 0, soc: false, gender: 0, ede: 0 };
+        return { study_num: currentCount, exam: "", scans: 0, soc: false, gender: "mixed", ede: 0 };
     }
 
 });
--- js/services.js
+++ js/services.js
@@ -11,6 +11,52 @@ angular.module("RadCalc.services", []).factory("getDataService", function($q, $h
       });
 
       return deferred.promise;
+    },
+
+    reformatJsonData: function(jsonData) {
+        var reformattedData = this.reformattedDataDefaults(jsonData);
+        var keysDoseData = Object.keys(jsonData.DoseData);
+        var examKey, examCategoryKey, exam, examCategoryIndex, examCategory;
+        var reformattedExamCategory;
+        for (examCategoryIndex in keysDoseData) {
+            examCategoryKey = keysDoseData[examCategoryIndex];
+            examCategory = jsonData.DoseData[examCategoryKey];
+            reformattedExamCategory = {"name":examCategoryKey, "exams": []};
+            for (examKey in examCategory) {
+              exam = {"name": examKey, "properties": []};
+              var examProperties = examCategory[examKey];
+              for (var propertyIndex in examProperties) {
+                  var propertyName = propertyIndex;
+                  var propertyGender = this.getGender(propertyName);
+                  var propertyValue = examProperties[propertyIndex];
+                  var property = {"name" :propertyName, "gender": propertyGender,"value": propertyValue};
+                  exam.properties.push(property);
+              }
+
+              reformattedExamCategory.exams.push(exam);
+            }
+            reformattedData.DoseData.push(reformattedExamCategory);
+        }
+        return reformattedData;
+    },
+  
+    reformattedDataDefaults: function(jsonData) {
+        var reformattedData = {};
+        reformattedData.ConsentNarrative = jsonData.ConsentNarrative;
+        reformattedData.ComparisonDoseSupportingLanguage = jsonData.ComparisonDoseSupportingLanguage;
+        reformattedData.ComparisonDose = jsonData.ComparisonDose;
+        reformattedData.DoseData = [];
+        return reformattedData;
+    },
+
+    getGender: function(propertyName) {
+      if (propertyName.indexOf("female") != -1) {
+        return "female";
+      }  else if (propertyName.indexOf("mixed") != -1) {
+        return "mixed";
+      } else {
+        return "male";
+      }
     }
   };
 });
